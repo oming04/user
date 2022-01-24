b064da621adcc73584c9c97674ff31e5b85ba9aa
oming4 <setyawan564@gmail.com>
2022-01-25 00:36:07 +0800

    first commit

---
+++ userbot/modules/ramcals.py
@@ -0,0 +1,92 @@
+# Thanks Full To Team Ultroid
+# Ported By Vcky @VckyouuBitch + @MaafGausahSokap
+# Copyright (c) 2021 Geez - Projects
+# Geez - Projects https://github.com/Vckyou/Geez-UserBot
+# RAM - UBOT https://github.com/ramadhani892/RAM-UBOT
+# Ini Belum Ke Fix Ya Bg :')
+# Ambil aja gapapa tp Gaguna kaya hidup lu Woakkakaka
+
+
+from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+
+from telethon.tl.types import ChatAdminRights
+from userbot import CMD_HELP
+from userbot.events import register
+
+NO_ADMIN = "`LU BUKAN ADMIN NGENTOT!!`"
+
+
+async def get_call(event):
+    rambot = await event.client(getchat(event.chat_id))
+    rama = await event.client(getvc(rambot.full_chat.call, limit=1))
+    return rama.call
+
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i: i + n]
+
+
+@register(outgoing=True, pattern=r"^\.startvc$")
+async def start_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await c.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        return
+    try:
+        await c.client(startvc(c.chat_id))
+        await c.edit("`OS DI MULAI, YG ONCAM LO ANJING!`")
+    except Exception as ex:
+        await c.edit(f"**ERROR:** `{ex}`")
+
+
+@register(outgoing=True, pattern=r"^\.stopvc$")
+async def stop_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await c.edit(f"**Maaf {ALIVE_NAME} Bukan Admin 👮**")
+        return
+    try:
+        await c.client(stopvc(await get_call(c)))
+        await c.edit("`OS DIMATIIN, TYPING AJA YA NGENTOT!`")
+    except Exception as ex:
+        await c.edit(f"**ERROR:** `{ex}`")
+
+@register(outgoing=True, pattern=r"^\.vcinvite", groups_only=True)
+async def _(rambot):
+    await rambot.edit("`Memulai Invite member group...`")
+    users = []
+    z = 0
+    async for x in rambot.client.iter_participants(rambot.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    hmm = list(user_list(users, 6))
+    for p in hmm:
+        try:
+            await rambot.client(invitetovc(call=await get_call(rambot), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await rambot.edit(f"`Menginvite {z} Member`")
+
+
+CMD_HELP.update(
+    {
+        "ramcalls": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.startvc`\
+         \n↳ : Memulai Obrolan Suara dalam Group.\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.stopvc`\
+         \n↳ : `Menghentikan Obrolan Suara Pada Group.`\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.vcinvite`\
+         \n↳ : Invite semua member yang berada di group. (Kadang bisa kadang kaga)."
+    }
+)
